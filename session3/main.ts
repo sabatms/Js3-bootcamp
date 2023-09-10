07e4afd479c795920cc4f43e04f3ddad840dcb17
sabatms <saba.tms15@gmail.com>
2023-09-10 11:35:10 -0700

    add main.ts file

---
+++ session3/main.ts
@@ -0,0 +1,25 @@
+let age1: number = 25;
+let age2: number = 10;
+
+if(age1 < age2 ){
+console.log("wellcome");
+}else{
+  console.log("you are young")
+};
+
+//
+
+let main1= 77;
+
+if (main1 > 90){
+  console.log("a");
+}else if (main1 > 80){
+  console.log("b");
+}else if (main1 > 70){
+  console.log("c");
+}else if (main1 > 60){
+  console.log("d");
+}else {
+  console.log("f");
+}
+//
