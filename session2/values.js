0d30f2092cc5b70d23ae4aa3bd3da3011e40be0c
sabatms <saba.tms15@gmail.com>
2023-09-10 07:13:59 -0700

    add values.js file

---
+++ session2/values.js
@@ -0,0 +1,29 @@
+//type script
+
+// numbers
+1000;
+20;
+0.7;
+25.276543;
+
+//string
+
+'hello world!';
+"welecome to my github";
+`this is saba `;
+/*
+هم به صورت  تک یا دبل کتیشن هست یا به صورت بگ تیک هم میتوان نوشت
+*/
+
+//boolean
+true;
+false;
+/*
+به صورت درست و غلط به ما نمایش میدهند
+*/
+
+null;
+// null  به صورت تعریف نشده به ما نشان داده میشود
+
+undefined;
+// یعنی نامشخص
