4eda9e330a31b2bfe3cd62de0dbef9d82b52ab1a
sabatms <saba.tms15@gmail.com>
2023-09-11 10:54:52 -0700

    add Assignmentoperator.js file

---
+++ session2/Assignmentoperator.js
@@ -0,0 +1,23 @@
+let counter = 9;
+counter = counter += 2;
+console.log ( counter);
+
+let main = 8;
+main = main *= 2;
+console.log ( main);
+
+let main1 = 18;
+main1 = main1 -= 4;
+console.log ( main1);
+
+let main2 = 24;
+main2 = main2 /= 2;
+console.log ( main2);
+
+let counter1 = 4;
+counter1++;
+console.log ( counter1);
+
+let counter2 = 4;
+counter2--;
+console.log ( counter2);
