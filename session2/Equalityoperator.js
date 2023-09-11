07d31d870bb00b964d051f64da5b07060cafcdbe
sabatms <saba.tms15@gmail.com>
2023-09-11 10:55:17 -0700

    add equality.js file

---
+++ session2/Equalityoperator.js
@@ -0,0 +1,32 @@
+console.log ( 3 == 5);
+// false 
+
+console.log ( 3 == 3);
+// true
+
+console.log ( 3 >= 8);
+// false
+
+console.log( 20 >= 14);
+// true
+
+console.log( 80 <= 100);
+// true
+
+console.log ( 10 === 10);
+// true
+
+console.log ( 20 === 10);
+// false 
+
+console.log ( 8 !== 8);
+// false
+
+console.log ( 6 !== "6");
+// true
+
+console.log ( 3 == "3");
+//  true
+
+console.log ( 3 !== "3");
+// true
