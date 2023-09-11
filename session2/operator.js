c6548a1595a80bf867613219f17aa5490c97f777
sabatms <saba.tms15@gmail.com>
2023-09-11 10:55:29 -0700

    add operator.js file

---
+++ session2/operator.js
@@ -0,0 +1,34 @@
+// عملگرها
+
+
+// Arithmetic operators : (+,-,*,/,**,%);
+
+console.log(20 + 24);
+console.log(1000 + 500);
+console.log(0.8 + 0.8);
+
+console.log(50 - 30);
+console.log(8 - 0.35);
+console.log(11.2 - 20);
+
+console.log(2 * 3);
+console.log(22.2 * 33.3);
+console.log(0.3 * 0.05);
+
+console.log( 10 / 2);
+console.log( 4000 / 0.2);
+console.log ( 500 / 0.15);
+
+console.log( 10 ** 2);
+console.log ( 2 ** 3);
+console.log ( 5 ** 3);
+
+console.log( 10 % 3);
+// (%) =  میاد حاصل باقی مانده رو به ا نشان میدهد
+
+console.log( "saba" + " tahmasebii");
+
+// NaN = Not a Number = یعنی عدد نیست
+
+console.log( 20 < 22);
+console.log ( 10 > 16); 
