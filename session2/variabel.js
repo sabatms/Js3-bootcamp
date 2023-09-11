618b80409c9c233e2dc8a9840f4630182580dc91
sabatms <saba.tms15@gmail.com>
2023-09-11 10:56:03 -0700

    add variabel.js file

---
+++ session2/variabel.js
@@ -0,0 +1,21 @@
+const age = 20 ;
+
+console.log ( age);
+
+const sum = 10 + 10;
+const sum1 = 22;
+
+console.log ( sum + sum1);
+console.log ( sum * sum1);
+console.log ( sum / sum1);
+console.log ( sum1 - sum);
+
+let name = "fatemeh";
+
+name = "saba";
+
+console.log (name);
+//  استفاده کنیlet باید از  valueاگر میخوای متغیری رو تغیر بدی مقدار 
+
+// const =  برای مقادیر ثابت هست
+
