9225fa1d812977f5cad974cbfdbd411863860e59
sabatms <saba.tms15@gmail.com>
2023-09-12 02:05:21 -0700

    add p1.ts file

--- session3/main.ts
+++ session3/main.ts
@@ -1,25 +1,8 @@
-let age1: number = 25;
-let age2: number = 10;
+let a :number = 20;
+let b : number = 14;
 
-if(age1 < age2 ){
-console.log("wellcome");
-}else{
-  console.log("you are young")
-};
-
-//
-
-let main1= 77;
-
-if (main1 > 90){
-  console.log("a");
-}else if (main1 > 80){
-  console.log("b");
-}else if (main1 > 70){
-  console.log("c");
-}else if (main1 > 60){
-  console.log("d");
+if ( a < b){
+  console.log( "your age is not true")
 }else {
-  console.log("f");
+  console.log ( "wellcome babe!")
 }
-//
