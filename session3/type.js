f0f0c64342f79dcdfc43b83bddedd8325ce487d0
sabatms <saba.tms15@gmail.com>
2023-09-12 01:08:01 -0700

    add type.js file

---
+++ session3/type.js
@@ -0,0 +1,15 @@
+const name = "mostafa";
+
+let a = 20;
+let b = 12;
+
+if(name === "mostafa"){
+a = a * 2;
+b = b * 3;
+}
+
+const price = a - b;
+const discount = price * 0.3
+
+console.log ( price);
+console.log(discount);
