d31f05f083af8571b54d3312ec7324b9fd6edaae
sabatms <saba.tms15@gmail.com>
2023-09-12 02:06:01 -0700

    add p4.ts file

---
+++ session3/p4.ts
@@ -0,0 +1,32 @@
+let Days : number = 7;
+
+switch(Days){
+          case 1:{
+          console.log("Saturday");
+          break
+          }
+          case 2:{
+          console.log ("Sunday");
+          break
+          }
+          case 3 :{
+          console.log("Monday");
+          break
+          }
+          case 4:{
+          console.log("Tusday");
+          break
+          }
+          case 5:{
+          console.log("Wedensday");
+          break
+          }
+          case 6:{
+          console.log ("Thursday");
+          break
+          }
+          case 7:{
+          console.log("Friday");
+          break
+          }
+}
