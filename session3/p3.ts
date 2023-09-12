05cc501464dc8be198ea25d7051679096944040a
sabatms <saba.tms15@gmail.com>
2023-09-12 02:05:48 -0700

    add p3.ts file

---
+++ session3/p3.ts
@@ -0,0 +1,16 @@
+let lights : string = "Red";
+
+switch (lights){
+          case"Red":{
+          console.log("the next is green");
+          break;
+          }
+          case"Yellow":{
+          console.log("the next is Red");
+          break
+          }
+          case "Green":{
+          console.log("the next is Yellow");
+          break
+          }
+}
