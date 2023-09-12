399ccd5a738b425deb680758c885fcb49787412c
sabatms <saba.tms15@gmail.com>
2023-09-12 01:07:31 -0700

    add switch.js file

---
+++ session3/switch.js
@@ -0,0 +1,17 @@
+//const name = "saba";
+
+const name = "elmira";
+switch(name){
+          case "vahid":
+          console.log( "are you vahid?");
+          break;
+          case "yasamin":
+          console.log ("are you yasamin?");
+          break;
+          case "elmira":
+          console.log(" are you elmira?");
+          break;
+          default:
+          console.log( "how is saba?");
+          break;
+}
