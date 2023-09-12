5ec4e87511b00b98d9fd0f07f99d30d5caba7845
sabatms <saba.tms15@gmail.com>
2023-09-12 01:07:16 -0700

    add if.js file

---
+++ session3/if.js
@@ -0,0 +1,55 @@
+const a =10;
+const b = 20;
+
+if( a===b){
+          console.log ( "true");
+}
+console.log( "false");
+//
+
+const c =10;
+const d = 10;
+
+if( c===d){
+ console.log ( "true");
+}else{
+          console.log( "cheek it");
+}
+
+//
+
+const e = 30;
+const f= 50;
+const name = "soha"
+if ( name === "ali"){
+          console.log ( "you are so sweet.")
+}else{
+          console.log( "this is not your name")
+}
+if ( e > f){
+          console.log ( "are you ok")
+}else{
+          console.log ( "not is true");
+}
+//
+
+const name1 = "elham";
+const name2 = "leila";
+
+if( name1 == "ali"){
+          console.log ( "hello elham");
+}else if( name1 == "tara"){
+          console.log("hello tara")
+}else{
+          console.log ("hello everyone!")
+}
+
+if ( name2 =="saba"){
+          console.log ( "how are you saba")
+}else if (name2 == "elahe"){
+          console.log("hello elahe");
+}else if ( name2 == "leila"){
+          console.log("Hi leila");
+}else {
+          console.log ( "who is leila?")
+};
