23ea831f284734089c413959065ca4e591ad4fd9
sabatms <saba.tms15@gmail.com>
2023-09-12 02:05:36 -0700

    add p2.ts file

---
+++ session3/p2.ts
@@ -0,0 +1,16 @@
+let grade : number = 20;
+if( grade > 100 && grade < 0){
+          console.log("not true");
+}else if (grade >= 90){
+          console.log("A")
+}else if (grade >=80){
+          console.log("B")
+}else if ( grade >=70){
+          console.log( "C")
+}else if ( grade >= 60){
+          console.log("D")
+}else if (grade >= 50){
+          console.log("E");
+}else {grade <= 50
+          console.log("F")
+}
