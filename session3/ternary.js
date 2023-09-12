d126234ced8b801c79ccad3a269fad4765ca4348
sabatms <saba.tms15@gmail.com>
2023-09-12 01:07:47 -0700

    add ternary.js file

---
+++ session3/ternary.js
@@ -0,0 +1,14 @@
+const name = "mehdi";
+
+let status = "";
+
+if (name === "mehdi") {
+ status ="hello,mehdii.";
+}else{
+status ="login"
+}
+
+let status2 = name =="mehdi"? "hello, mehdii!":"login";
+console.log(status2);
+
+// ternary = سه گانه 
