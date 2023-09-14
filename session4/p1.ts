1952811a9ad39256c2e210458153f92b80e12696
sabatms <saba.tms15@gmail.com>
2023-09-13 22:59:30 -0700

    add p1.ts file

---
+++ session4/p1.ts
@@ -0,0 +1,10 @@
+function area( radius:number):number {
+          const p = 3.14;
+          const result = 2 * p * radius;
+          const result1 = (result/2 )+(2* radius)
+
+          return result1;
+}
+
+const a= area( 10);
+console.log(a);
