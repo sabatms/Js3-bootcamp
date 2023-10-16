e4d1d3b4e11afbf48f134ccc916bf438d0b28774
sabatms <saba.tms15@gmail.com>
2023-10-16 11:48:52 -0700

    add dist file

---
+++ dist/about.2040185b.js
@@ -0,0 +1,3 @@
+console.log("About page!");
+
+//# sourceMappingURL=about.2040185b.js.map
---
+++ dist/index.87f81111.js
@@ -0,0 +1,4 @@
+console.log("Hello world!");
+console.log("i'm from LDN");
+
+//# sourceMappingURL=index.87f81111.js.map
