84ca4bd5996b57823ce95375610ccfef9c10d1d2
sabatms <saba.tms15@gmail.com>
2023-10-21 09:57:09 -0700

    add dist file

--- dist/index.87f81111.js
+++ dist/index.87f81111.js
@@ -1,4 +1,7 @@
 console.log("Hello world!");
 console.log("i'm from LDN");
+setTimeout(function() {
+    console.log("saba tahmasebi nezhad");
+}, 2000);
 
 //# sourceMappingURL=index.87f81111.js.map
