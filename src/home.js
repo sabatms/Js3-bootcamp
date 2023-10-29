f0b6e0670b40f976629c1bd488291b4bb0731369
sabatms <saba.tms15@gmail.com>
2023-10-29 04:16:47 -0700

    add home.js file in src

--- src/home.js
+++ src/home.js
@@ -9,8 +9,33 @@ document.querySelector("button").addEventListener("click",increase);
 
 
 //const btn = document.querySelector("button");
+const name ="sina";
 const btn = document.querySelector("#saba");
 const inp = document.querySelector("input[name = firstname]");
+
+// setTimeout(()=>{
+//           inp.value = name;
+//           btn.textContent ="send";
+// },2000);
+
+
+// setInterval(() =>{
+//   btn.classList.toggle("success");
+// },1000);
+
+btn.addEventListener("click", () => {
+          console.log("clicked!")
+});
+
+inp.addEventListener("keydown", ()=>{
+          if(inp.value === ""){
+          console.log("empty");
+          btn.classList.remove("success");
+          }else{
+          console.log("ok");
+          btn.classList.add("success");
+          }
+})
 //console.log({ btn });
 //console.log(btn.textContent);
 
