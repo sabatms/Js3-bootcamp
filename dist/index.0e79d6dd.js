b3ac9431b963d3a3d7c2451b0ca6e89cdce9458a
sabatms <saba.tms15@gmail.com>
2023-10-30 01:14:03 -0700

    add dist file

--- dist/index.0e79d6dd.js
+++ dist/index.0e79d6dd.js
@@ -585,17 +585,45 @@ document.querySelector("button").addEventListener("click",increase);
 const name = "sina";
 const btn = document.querySelector("#saba");
 const inp = document.querySelector("input[name = firstname]");
-// setTimeout(()=>{
-//           inp.value = name;
-//           btn.textContent ="send";
-// },2000);
-// setInterval(() =>{
-//   btn.classList.toggle("success");
-// },1000);
-btn.addEventListener("click");
-//console.log({ btn });
-//console.log(btn.textContent);
-console.log(inp.value);
+const password_input = document.querySelector("input[name=pw]");
+const description_element = document.querySelector(".description");
+const my_p = document.createElement("p");
+my_p.classList.add("succses");
+my_p.textContent = "salam in yek matn ast";
+description_element.appendChild(my_p);
+console.log(my_p);
+const my_p1 = document.createElement("p");
+my_p1.textContent = "saba tahmasebi";
+description_element.appendChild(my_p1);
+console.log(my_p1); // password_input.setAttribute("type","password");
+ // password_input.setAttribute("value","saba");
+ // //password_input.setAttribute("class","sucsses salam");
+ // password_input.classList.add("success","hello");
+ // console.log({
+ //  password_input,
+ // });
+ // setTimeout(()=>{
+ //           inp.value = name;
+ //           btn.textContent ="send";
+ // },2000);
+ // setInterval(() =>{
+ //   btn.classList.toggle("success");
+ // },1000);
+ // btn.addEventListener("click", () => {
+ //           console.log("clicked!")
+ // });
+ // inp.addEventListener("keydown", ()=>{
+ //           if(inp.value === ""){
+ //           console.log("empty");
+ //           btn.classList.remove("success");
+ //           }else{
+ //           console.log("ok");
+ //           btn.classList.add("success");
+ //           }
+ // })
+ // console.log({ btn });
+ // console.log(btn.textContent);
+ // console.log(inp.value);
 
 },{}]},["41yKj","dVZgU"], "dVZgU", "parcelRequire6a6d")
 
